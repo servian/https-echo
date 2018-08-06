999885f0ad98f9ea68b477fa3db876f96f71f596
Tristan Morgan <tristanmorgan@users.noreply.github.com>
2018-08-06 22:36:39 +1000

    Add version and listen cli-flags.

--- main.go
+++ main.go
@@ -1,9 +1,19 @@
 package main
 
 import (
+	"flag"
+	"fmt"
 	"log"
 	"net/http"
+	"os"
 	"strings"
+)
+
+const Version = "0.0.1"
+
+var (
+	httpAddr = flag.String("listen", ":80", "Listen address")
+	versDisp = flag.Bool("version", false, "Display version")
 )
 
 func redirect(w http.ResponseWriter, req *http.Request) {
@@ -12,11 +22,18 @@ func redirect(w http.ResponseWriter, req *http.Request) {
 	if len(req.URL.RawQuery) > 0 {
 		target += "?" + req.URL.RawQuery
 	}
-	log.Printf("redirect to: %s", target)
+	log.Printf("redirect to: %s from: ", target, req.RemoteAddr)
 	http.Redirect(w, req, target,
 		http.StatusTemporaryRedirect)
 }
 
 func main() {
-	http.ListenAndServe(":80", http.HandlerFunc(redirect))
+	flag.Parse()
+
+	if *versDisp {
+		fmt.Printf("Version: v%s\n", Version)
+		os.Exit(0)
+	}
+
+	http.ListenAndServe(*httpAddr, http.HandlerFunc(redirect))
 }
