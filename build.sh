1fe7e5885fe05f0258f9290cde6a673c992cecf5
Tristan Morgan <tristanmorgan@users.noreply.github.com>
2018-08-28 10:24:52 +1000

    Satisfy GoLint

--- build.sh
+++ build.sh
@@ -1,8 +1,6 @@
 #!/bin/sh
 
 docker build -t vibrato/https-echo .
-docker run --rm \
- -v ${PWD}:/go/src/github.com/vibrato/https-echo \
- -w /go/src/github.com/vibrato/https-echo \
- -e CGO_ENABLED=0 -e GOOS="linux" -e GOARCH="amd64" \
- golang:alpine go build -a -ldflags="-s -w" -o https-echo
+docker create --name https-echo vibrato/https-echo
+docker cp https-echo:/https-echo .
+docker rm https-echo
--- main.go
+++ main.go
@@ -9,6 +9,7 @@ import (
 	"strings"
 )
 
+// Version number constant.
 const Version = "0.0.3"
 
 var (
